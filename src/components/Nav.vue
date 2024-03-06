f97f6c39db170bb9739c27ddc2ae34d7f14ddbcf
Anggaranur <m.anggaranurakbar@gmail.com>
2024-03-06 08:49:26 +0700

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="w-screen sticky shadow-xl h-[5rem]">
-    <a href="/service">
+    <a href="/Contact">
       <img class="w-[12rem] cursor-pointer" src="@/assets/img/ManaIT-Logo.png">
     </a>
   </nav>
